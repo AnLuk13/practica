c94f4446a0bdfa69f929a96240a7fbfd099df0e5
unknown <vmorari@fusionworks.md>
2023-09-11 11:56:28 +0300

    React init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const dummyData = [
+  { id: "123", name: "test1" },
+  { id: "456", name: "test2" },
+  { id: "789", name: "test3" },
+];
 
 function App() {
+  const [_counter, _setCounter] = useState(0);
+
+  const incriment = () => {
+    _setCounter(_counter + 1);
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Count: {_counter}</div>
+      <button onClick={incriment}>+</button>
+      {dummyData.map((el) => {
+        return (
+          <div>
+            id:{el.id}- Name:{el.name}
+          </div>
+        );
+      })}
     </div>
   );
 }
