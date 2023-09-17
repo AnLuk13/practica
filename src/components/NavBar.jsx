bdb9faeebfe10e504509ee0e7cbc0f97eaee5296
Antonio <45819634+AnLuk13@users.noreply.github.com>
2023-09-17 15:42:58 +0300

    Style home page and navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom";
+import "../styles/NavBar.css";
+import logo from "../img/svg/vector-2.svg";
+import search from "../img/svg/vector-1.svg";
 
 export default function Navbar() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about-us">About Us</Link>
-          </li>
-        </ul>
+      <nav className="nav-bar">
+        <div className="nav-bar__links">
+          <Link to="/">
+            <img src={logo} alt="" />
+          </Link>
+          <Link className="nav-bar__link selected" to="/">
+            Home
+          </Link>
+          <Link className="nav-bar__link" to="/">
+            Hairdress
+          </Link>
+          <Link className="nav-bar__link" to="/">
+            Tattoo
+          </Link>
+          <Link className="nav-bar__link" to="/about-us">
+            About Us
+          </Link>
+          <img src={search} alt="" />
+        </div>
       </nav>
     </>
   );
